4474c3b70bdc0660e182141fd7a733f41440fa3b
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-06-19 19:31:30 +0300

    Update CakeContainer.jsx

--- src/components /CakeContainer.jsx
+++ src/components /CakeContainer.jsx
@@ -14,7 +14,6 @@ export default CakeContainer;
 //1. how to dispatch action?
 //2. how to access to redux store?
 
+//without hooks (CC) => mapDispatchToProps => BAD PRACTICE
 
-//without hooks (CC) => mapDispatchToProps => BAD PRACTICE 
-
-//instead useSelectore and useDispatch 
+//instead useSelectore and useDispatch
