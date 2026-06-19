e5f3451a89a2942e76288a260b92b3ef654b4b50
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-06-19 19:31:22 +0300

    Add notes on Redux usage in CakeContainer

--- src/components /CakeContainer.jsx
+++ src/components /CakeContainer.jsx
@@ -13,3 +13,8 @@ export default CakeContainer;
 
 //1. how to dispatch action?
 //2. how to access to redux store?
+
+
+//without hooks (CC) => mapDispatchToProps => BAD PRACTICE 
+
+//instead useSelectore and useDispatch 
