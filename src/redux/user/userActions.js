39865486ac429e8a01dabc91a03f2b2e7cf8bf7d
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-06-22 20:01:52 +0300

    Add action creators and refactor fetchUsers

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -4,23 +4,43 @@ import {
   FETCH_USERS_SUCCESS,
 } from "./userTypes";
 
+function fetchUsersRequest() {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+}
+
+function fetchUserSuccess(users) {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    playload: users,
+  };
+}
+
+function fetchUsersFailure(error) {
+  return {
+    type: FETCH_USERS_FEILURE,
+    playload: error,
+  };
+}
+
 export function fetchUsers() {
   return (dispatch) => {
-    dispatch({ type: FETCH_USERS_REQUEST });
+    //dispatch({ type: FETCH_USERS_REQUEST });
+    dispatch(detchUsersRequest());
     axios
       .get("https://jsonplaceholder.typicode.com/users")
-      .then((res) => dispatch({ type: FETCH_USERS_SUCCESS, payload: res.data }))
+      .then((res) => {
+        //dispatch({ type: FETCH_USERS_SUCCESS, payload: res.data })
+        dispatch(fetchUserSuccess(res.data));
+      })
       .catch((err) =>
-        dispatch({
-          type: FETCH_USERS_FAILURE,
-          playload: err.response.data.message,
-        }),
+        //dispatch({ type: FETCH_USERS_FAILURE, playload: err.response.data.message,}),
+        dispatch(fetchUsersFailure(err.message)),
       );
   };
 }
 
 //export const fetchUsers = () => () => {};
-
-
 
 // think => past tense (thought) => delay (middleware)
